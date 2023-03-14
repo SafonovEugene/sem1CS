70640ef10bb95bfc9d3a155e7ec514f104493711
Eugene <safonov77@mail.ru>
2023-03-14 06:14:48 +0300

    Added code C#

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿//Определение большего из двух чисел
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+//         if (a == b)
+//         {
+//             Console.WriteLine("Числа равны");
+//         }
+//         else
+        
+//         if (a > b)
+//         {
+//             Console.WriteLine(a);
+//         }
+//         else
+//         {
+//             Console.WriteLine(b);
+//         }
+
+// Определение большего из трех чисел
+// Console.Write("Введите число: ");
+//          int number = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число: ");
+//          int number2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число: ");
+//          int number3 = int.Parse(Console.ReadLine()!),
+        
+//         maximum = number;
+//         if (number2 > maximum)
+//         {
+//             maximum = number2;
+//             if (number3 > maximum)
+//             {
+//                 maximum = number3;
+//             }
+//         }
+//         else
+//         {
+//             if (number3 > maximum)
+//             {
+//                 maximum = number3;
+//             }
+//         }
+//         Console.WriteLine($"Максимальное число из 3: {maximum}");
+
+// ОПРЕДЕЛЕНИЕ ЧЕТНОСТИ
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!),
+        
+//         if (n % 2 == 0)
+//         {
+//             Console.WriteLine("parity");
+//         }
+//         else
+//         {
+//             Console.WriteLine("none parity");
+//         }
+
+
+//Определение четных чисел от 1 до Эн
+// Console.Write("Введите число: ");
+//         int n = int.Parse(Console.ReadLine()!),
+//         index = 0;
+        
+//         while (index < n)
+//         {
+//             index++;
+//             if (index % 2 == 0)
+//             {
+//                 Console.WriteLine(index);
+//             }        
+//         }
+
